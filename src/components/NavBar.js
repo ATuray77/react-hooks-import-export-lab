9268039fe3149132be44f770b6df06a23343ec33
Aruna Turay <tura_arun@yahoo.com>
2023-05-18 17:42:09 -0400

    import export

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import About from "./About";
 
 function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#About" />
     </nav>
   );
 }
+
+export default NavBar
